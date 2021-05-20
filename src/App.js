6f84062fb334592b5e93767beda0ef19a6529422
Gustavo Farias <gustavogarcia266@hotmail.com>
2021-05-20 03:21:50 -0300

    adicionando as piadas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React  from "react";
+import React, { useEffect, useState }  from "react";
 import './index.css';
 
 const API_URL = `http://api.icndb.com/jokes/random`;
 
 function App() {
+
+    const [joke, setJoke] = useState('');
+
+    const generateJoke = () => {
+      fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setJoke(data.value.joke));
+    }
+
+    useEffect(() => {
+      generateJoke();
+    }, [])
+
     return (
-      <div>
-         <h1>Hello</h1>
+      <div className="box">
+         <h1>Jokes Generator</h1>
+         <p>{joke}</p>
+         <button onClick={generateJoke}>Get new joke 😂</button>
       </div>
     );
   }
